72c503bae36db0aeff2fc95ba4ab80a116bd0168
lakshithaiam <lakshithaiam@gmil.com>
2025-04-19 12:21:29 +0530

    new update2

--- flow.sh
+++ flow.sh
@@ -16,9 +16,16 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm show values bitnami/zookeeper > zookeeper-values-v1.yaml
 helm show values prometheus-community/kube-prometheus-stack > kube-prometheus-values-v1.yaml
 
-https://hooks.slack.com/services/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 << new lakshithanoone
+https://hooks.slack.com/services/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 << lakshithanoone
+https://hooks.slack.com/services/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
+#slack-all
+#slack-critical
+#slack-clickhouse
 
-kube-prometheus-values-v1.yaml  zookeeper-values-v1.yaml
+https://hooks.slack.com/services/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 << v4c 
+#test-alerts
+
+
 
 helm upgrade --install my-zookeeper bitnami/zookeeper -f zookeeper-values-v1.yaml -n zookeeper
 helm upgrade --install my-kube-prometheus-stack prometheus-community/kube-prometheus-stack -f kube-prometheus-values-v1.yaml -n monitoring
